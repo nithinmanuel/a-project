d31ca6a272b24db48ab288a35e13e320550e4b90
nithinmanuel <nithinmanuel9@gmail.com>
2018-11-05 16:23:45 +0100

    a-project

--- db/migrate/20181102153351_create_products.rb
+++ db/migrate/20181102153351_create_products.rb
@@ -5,9 +5,10 @@ class CreateProducts < ActiveRecord::Migration[5.2]
       t.string :meta_title
       t.text :description
       t.timestamp
-      #t.string :my_default_proc, :datetime, default: -> { Time.now }
+      t.string :datetime -> { Time.now }
     end
   end
 end
 
 
+#t.string :my_default_proc, :datetime, default: -> { Time.now }
