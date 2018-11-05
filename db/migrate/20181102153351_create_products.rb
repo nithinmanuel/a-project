67819cf3b5785bac0d0087c12ec622817901eb74
nithinmanuel <nithinmanuel9@gmail.com>
2018-11-05 10:28:09 +0100

    a-project

--- db/migrate/20181102153351_create_products.rb
+++ db/migrate/20181102153351_create_products.rb
@@ -7,18 +7,5 @@ class CreateProducts < ActiveRecord::Migration[5.2]
       t.timestamp
       attribute :my_default_proc, :datetime, default: -> { Time.now }
   end
-      #t.strftime(2007,11,19,8,37,48,"-06:00") 
       
-  
-      
-  	# :products do |d|
-     # d = DateTime.new(2007,11,19,8,37,48,"-06:00")ime
-##=> #<DateTime: 2007-11-19T08:37:48-0600 ...>
-      #d.strftime("Printed on %m/%d/%Y")   #=> "Printed on 11/19/2007"
-      #d.strftime("at %I:%M%p")
-    #t = Time.n
-
-
-#<td><%= product.strftime("%B %d %Y, %l:%M%P") %></td>
-#<td><%= product.Time %></td>.
 
